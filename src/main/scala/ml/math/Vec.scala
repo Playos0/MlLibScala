2a6d8edb8d5b882a67180bc9e46c85131dd3b946
Playos0 <krzysiek.1.k@wp.pl>
2026-02-04 23:46:44 +0100

    Dodano do Vec.scala
    Dodano podstawowe działania na wektorach

---
+++ src/main/scala/ml/math/Vec.scala
@@ -0,0 +1,38 @@
+package ml.math
+
+import javax.print.attribute.standard.MediaSize.Other
+
+opaque type Vec = Vector[Double]
+
+object Vec{
+
+  //konstruktor do Vec
+
+  def apply(values: Double*): Vec =
+    values.toVector
+
+
+  //metody do typu Vec (rozszerzenie)
+  extension(v: Vec)
+
+    def size: Int =
+      v.length
+
+
+    def +(other: Vec): Vec = //definicja metody dodawania dla obiektu Vec
+      require(v.length == other.length) //sprawdza by dwa wektory były równej długości
+      v.zip(other).map(_ + _)
+
+    def *(scalar: Double): Vec =
+      v.map(_ * scalar)
+
+    def dot(other: Vec): Double =
+      require(v.length == other.length)
+      v.zip(other).map(_ * _).sum
+
+    def norm: Double =
+      math.sqrt(v.dot(v))
+}
+
+
+
