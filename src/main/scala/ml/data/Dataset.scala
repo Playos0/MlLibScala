3400bb2eafce47f309a5e104dafe0d8f4749ee69
Playos0 <krzysiek.1.k@wp.pl>
2026-02-04 23:57:50 +0100

    Dodano do Dataset.scala
    Zmienione Dataset przygotowane do bardzo podstawowej pracy na danych

--- src/main/scala/ml/data/Dataset.scala
+++ src/main/scala/ml/data/Dataset.scala
@@ -1,5 +1,22 @@
 package ml.data
 
-case class Dataset[X,Y](features: Vector[X], labels: Vector[Y]){
-  require(features.size == labels.size)
+import ml.math.Vec
+
+
+//ta klasa przechowuje cechy i etykiety danych
+case class Dataset(features: Vector[Vec], //każda obserwacja to wektor cech
+                   labels: Vector[Double]) { // a to odpowiadające im etykiety
+  require(features.size == labels.size, "Liczba cech musi odpowiadać liczbie etykiet")
+
+  def size: Int = features.size
+
+  //dzieli na train/test (na ten moment łatwa wersja)
+  def split(ratio: Double = 0.8): (Dataset, Dataset) = {
+    val splitIndex = (features.size * ratio).toInt
+    val train = Dataset(features.take(splitIndex), labels.take(splitIndex))
+    val test = Dataset(features.drop(splitIndex), labels.drop(splitIndex))
+    (train, test)
+  }
 }
+
+
